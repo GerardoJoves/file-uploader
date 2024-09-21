7df3964b52653bc602e10a2ba231675103b6dfe6
Gerardo Joves <gerardooenrrique@gmail.com>
2024-09-21 02:47:54 -0400

    refactor(validation): Improve organization

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -7,26 +7,31 @@ export type UserSignupData = {
   passwordConfirmation: string;
 };
 
-const userSignupValidation = [
+const isUsernameAvailable = async (username: string) => {
+  const user = await prisma.user.findUnique({
+    where: { username: username },
+  });
+  if (user) throw new Error('Username already in use');
+};
+
+const usernameValidaton = () =>
   body('username')
     .trim()
     .isLength({ min: 4, max: 25 })
     .withMessage('Username must be 4 to 25 characters long')
     .isAlphanumeric()
-    .withMessage('Username must contain only letters, numbers, or underscores')
-    .custom(async (value: string) => {
-      const user = await prisma.user.findUnique({
-        where: { username: value },
-      });
-      if (user) throw new Error('Username already in use');
-    }),
+    .withMessage('Username must contain only letters, numbers, or underscores');
+
+const passwordValidation = () =>
   body('password')
     .isLength({ min: 8, max: 30 })
     .withMessage('Password must be 8 to 30 characters long')
     .matches(/^(?=.*\d)(?=.*[a-zA-Z]).+$/)
-    .withMessage(
-      'Password must contain at least one number, one letter and be 8 to 30 characters long ',
-    ),
+    .withMessage('Password must contain at least one number and one letter');
+
+const userSignupValidation = [
+  usernameValidaton().custom(isUsernameAvailable),
+  passwordValidation(),
   body('passwordConfirmation')
     .custom((value, { req }) => {
       const { password } = req.body as UserSignupData;
