f0dee81fedd873a4e6bd57a37366138167edace0
Gerardo Joves <gerardooenrrique@gmail.com>
2024-10-25 18:46:17 -0400

    refactor modal: make it possible to display file details

--- src/client/index.ts
+++ src/client/index.ts
@@ -7,34 +7,37 @@ type ModalOperation =
   | 'renameFile'
   | 'deleteFolder'
   | 'deleteFile'
+  | 'fileDetails'
   | '';
 
 interface ModalState {
-  submitted: false;
   isOpen: boolean;
-  endpoint: string;
+  endpoint?: string;
   operation: ModalOperation;
   values: object;
+}
+
+interface ModalOpenParams {
+  operation: ModalOperation;
+  endpoint?: string;
+  values?: object;
 }
 
 const Alpine = alpinejs as unknown as Alpine;
 
 Alpine.store('modal', <ModalState>{
-  submitted: false,
   isOpen: false,
-  endpoint: '',
   operation: '',
   values: {},
 
-  open(operation: ModalOperation, endpoint: string, values?: object) {
+  open({ operation, endpoint, values }: ModalOpenParams) {
     this.isOpen = true;
     this.operation = operation;
     this.endpoint = endpoint;
-    if (values) this.values = values;
+    this.values = values || {};
   },
 
   close() {
-    this.submitted = false;
     this.isOpen = false;
     this.endpoint = '';
     this.operation = '';
