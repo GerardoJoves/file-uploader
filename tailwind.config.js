92cfc114b59cd6c143ee6448fcb2edc54c6d064f
Gerardo Joves <gerardooenrrique@gmail.com>
2024-10-25 21:12:53 -0400

    Implement responsive table layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ export default {
       boxShadow: {
         lg: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
       },
+      screens: {
+        xxs: '400px',
+        xs: '550px',
+        lg: '950px',
+        xl: '1100px',
+        '2xl': '1300px',
+      },
     },
   },
   plugins: [],
